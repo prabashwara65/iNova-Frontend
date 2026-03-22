1778a4a94772ed51c02150c8545764a36976fa1a
Priyanka Prabashwara <prabashwara65@gmail.com>
2026-03-22 14:16:52 +0530

    commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 const API_BASE =
   import.meta.env.VITE_API_BASE_URL ||
-  "https://inova-alb-1159271538.eu-north-1.elb.amazonaws.com/";
+  "https://inova-alb-1159271538.eu-north-1.elb.amazonaws.com";
 
 async function handleResponse(response) {
   const contentType = response.headers.get("content-type") || "";
